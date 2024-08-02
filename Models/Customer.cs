e9a467f0fd79c01ae055b6f8b4c6e9beb735df73
Tu Nombre <you@example.com>
2024-08-02 07:22:38 -0500

    Fix: Se le agrego formato a los elementos del constructor en la clase Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -11,8 +11,8 @@ namespace Simulacro_01.Models
         public string PreferredPaymentMethod { get; set; }
         public Customer(string _name, string _lastName, string _typeDocument, string _identificationNumber, DateOnly _birthDate, string _email, string _phoneNumber, string _address, string _membershipLevel, string _preferredPaymentMethod) : base(_name, _lastName, _typeDocument, _identificationNumber, _birthDate, _email, _phoneNumber, _address)
         {
-            this.MembershipLevel = _membershipLevel;
-            this.PreferredPaymentMethod = _preferredPaymentMethod;
+            this.MembershipLevel = _membershipLevel.Trim();
+            this.PreferredPaymentMethod = _preferredPaymentMethod.Trim().ToLower();
         }
         public void UpdateMembershipLevel(){
             MembershipLevel +=1;
