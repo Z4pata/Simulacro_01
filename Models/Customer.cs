e0e401c3db08465cd8a54e1fe77960c9fb094aed
Tu Nombre <you@example.com>
2024-08-02 07:10:55 -0500

    Feat: Se creo la clase Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacro_01.Models
+{
+    public class Customer : User
+    {
+        public string MembershipLevel { get; set; }
+        public string PreferredPaymentMethod { get; set; }
+        public Customer(string _name, string _lastName, string _typeDocument, string _identificationNumber, DateOnly _birthDate, string _email, string _phoneNumber, string _address, string _membershipLevel, string _preferredPaymentMethod) : base(_name, _lastName, _typeDocument, _identificationNumber, _birthDate, _email, _phoneNumber, _address)
+        {
+            this.MembershipLevel = _membershipLevel;
+            this.PreferredPaymentMethod = _preferredPaymentMethod;
+        }
+        public void UpdateMembershipLevel(){
+            MembershipLevel +=1;
+        }
+    }
+}
