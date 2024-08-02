a320e27eb052e7f258685a6401b35b8dcfbb9e14
Tu Nombre <you@example.com>
2024-08-02 07:19:30 -0500

    Feat: Se inicializo la clase Vehicle (Pendiente de actualizar)

---
+++ Models/Vehicle.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacro_01.Models
+{
+    public class Vehicle
+    {
+
+        public int Id { get; set; }
+        public string Plate { get; set; }
+        public string Type { get; set; }
+        public string EngineNumber { get; set; }
+        public string SerialNumber { get; set; }
+        public byte PeopleCapacity { get; set; }
+        public Driver Owner { get; set; }
+        public Vehicle(int _id, string _plate, string _type, string _engineNumber, string _serialNumber, byte _peopleCapacity, Driver _owner)
+        {
+            this.Id = _id;
+            this.Plate = _plate;
+            this.Type = _type;
+            this.EngineNumber = _engineNumber;
+            this.SerialNumber = _serialNumber;
+            this.PeopleCapacity = _peopleCapacity;
+            this.Owner = _owner;
+        }
+        public void DeleteVehicle(int id){
+            
+        }
+    }
+}
