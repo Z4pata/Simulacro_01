22e95a4dcca9b08fec2bc04a7a9a86c33bb740f9
Tu Nombre <you@example.com>
2024-08-02 11:47:41 -0500

    Fix: Se Cambio el constructor y el metodo ShowDriver

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -14,7 +14,7 @@ namespace Simulacro_01.Models
         public Driver(string _name, string _lastName, string _typeDocument, string _identificationNumber, DateOnly _birthDate, string _email, string _phoneNumber, string _address, string _licenseNumber, string _licenseCategory, int _drivingExperience) : base(_name, _lastName, _typeDocument, _identificationNumber, _birthDate, _email, _phoneNumber, _address)
         {
             this.LicenseNumber = _licenseNumber.Trim();
-            this.LicenseCategory = _licenseCategory.Trim();
+            this.LicenseCategory = _licenseCategory.Trim().ToUpper();
             this.DrivingExperience = _drivingExperience;
         }
         public void UpdateLicenseCategory(string newCategory){
@@ -26,7 +26,7 @@ namespace Simulacro_01.Models
 
         public void ShowDriver(){
             ShowDetails();
-            Console.WriteLine(@$"           Numero de licencia: {LicenseNumber}
+            Console.WriteLine(@$"            Numero de licencia: {LicenseNumber}
             Categoria de la licencia: {LicenseCategory}
             Años de experiencia: {DrivingExperience}");
         }
