d3acc921c35f4ed6df8c812fd1233dcc9c5f90c8
Tu Nombre <you@example.com>
2024-08-02 12:04:57 -0500

    Feat: Se Creo el metodo para crear un vehiculo

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -173,15 +173,16 @@ namespace Simulacro_01.Models
             string lastName;
             string typeDocument;
             string identificationNumber;
-            int year = 0;
-            int month = 0;
-            int day = 0;
             DateOnly birthDate;
             string email;
             string phoneNumber;
             string address;
             string licenseNumber;
             string licenseCategory;
+            // Les tengo que dar un valor ya que al hacer las validaciones me da un error de que la variable no tiene contenido
+            int year = 0;
+            int month = 0;
+            int day = 0;
             int drivingExperience = 0;
 
 
@@ -343,6 +344,114 @@ namespace Simulacro_01.Models
             var driver = new Driver(name, lastName, typeDocument, identificationNumber, birthDate, email, phoneNumber, address, licenseNumber, licenseCategory, drivingExperience);
 
             return driver;
+        }
+
+        public static Vehicle CreateVehicle(int _id)
+        {
+            string plate;
+            string type;
+            string engineNumber;
+            string serialNumber;
+            // Les tengo que dar un valor ya que al hacer las validaciones me da un error de que la variable no tiene contenido
+            byte peopleCapacity = 0;
+            Driver owner = new Driver("sa", "sad", "sa", "ad", new DateOnly(202, 1, 10), "sda", "sad", "sad", "asd", "sda", 12);
+
+            // Placa
+
+            do
+            {
+                Console.Write("Ingrese el numero de placa: ");
+                plate = Console.ReadLine();
+
+            } while (string.IsNullOrWhiteSpace(plate));
+
+            // tipo de carro
+
+            do
+            {
+                Console.WriteLine(@"    Tipo de Vehiculo");
+                Console.Write(@$"
+                - Carro
+                - Moto
+                - Camioneta
+                - Microbus
+                
+                => ");
+                type = Console.ReadLine().ToLower();
+            } while (Settings.ValidateVehicleType(type));
+
+            // Numero del motor
+
+            do
+            {
+                Console.Write("Ingrese el numero del motor: ");
+                engineNumber = Console.ReadLine();
+
+            } while (string.IsNullOrWhiteSpace(engineNumber));
+
+            // Numero de serie
+
+            do
+            {
+                Console.Write("Ingrese el numero de serie: ");
+                serialNumber = Console.ReadLine();
+
+            } while (string.IsNullOrWhiteSpace(serialNumber));
+
+            // Capacidad del vehiculo
+
+            string sCapacity;
+
+            do
+            {
+                Console.Write("Cuantas personas caben?: ");
+                sCapacity = Console.ReadLine();
+                if (!Settings.ValidateByte(sCapacity))
+                {
+                    peopleCapacity = Convert.ToByte(sCapacity);
+                }
+                else
+                {
+                    VisualInterfaces.ShowIntErrorMessage();
+                }
+
+            } while (Settings.ValidateByte(sCapacity));
+
+            // Dueño del vehiculo
+            bool flag = true;
+            while (flag)
+            {
+                string idOwner;
+                do
+                {
+                    Console.Write("Ingrese el numero de identificacion del dueño: ");
+                    idOwner = Console.ReadLine();
+
+                } while (string.IsNullOrWhiteSpace(idOwner));
+
+                if (Storage.DriverList.Any(d => d.IdentificationNumberPublic == idOwner))
+                {
+                    owner = Storage.DriverList.FirstOrDefault(d => d.IdentificationNumberPublic == idOwner);
+                    flag = false;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No se encontró ningun conductor con esa identificacion!!");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Crealo!!");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    var driver = Storage.AddDriver();
+                    owner = driver;
+                    flag = false;
+                }
+            }
+
+            var vehicle = new Vehicle(_id, plate, type, engineNumber, serialNumber, peopleCapacity, owner);
+
+            return vehicle;
+
         }
     }
 }
